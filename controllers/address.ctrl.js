fb386728087bebe5e4d39587fe665b507a54c79a
윤철중 <cjfwnd123zz@gmail.com>
2021-07-21 22:07:38 +0900

    add: 유저의 기준지역, 활동범위 불러오기 기능 추가

--- controllers/address.ctrl.js
+++ controllers/address.ctrl.js
@@ -1,1 +1,13 @@
 const model = require("../models");
+
+exports.get_address = async (req, res, next) => {
+  try {
+    const userAddress = await model.UserAddress.findOne({ where: { userId: req.user.id } });
+    const addressScope = userAddress.addressScope;
+    const district = await model.District.findOne({ where: { id: userAddress.districtId } });
+    const simpleAddress = district.simpleAddress;
+    res.status(200).json({ success: true, message: "회원의 기준지역과 활동범위를 반환합니다.", simpleAddress, addressScope });
+  } catch (e) {
+    return next(e);
+  }
+};
--- routes/address.router.js
+++ routes/address.router.js
@@ -2,13 +2,16 @@ const { Router } = require("express");
 const router = Router();
 const ctrl = require("../controllers");
 const vaild = require("../vaildators");
+const middleware = require("../middlewares");
 
 function addressRouter(root) {
   root.use("/address", router);
+  router.use(middleware.auth.isAuthenticated);
   /**
-   * @description
-   * @routes
+   * @description 유저가 설정한 기준지역, 범위 불러오기
+   * @routes GET /address
    */
+  router.get("/", ctrl.address.get_address);
 }
 
 module.exports = addressRouter;
