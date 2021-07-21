09d571a11717e883f5e94cb91aba16eeac4ebf89
윤철중 <cjfwnd123zz@gmail.com>
2021-07-21 22:20:58 +0900

    chore: auth의 vaildator 변수명 변경

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -9,7 +9,7 @@ function authRouter(root) {
    * @description 회원가입
    * @routes POST /auth/signup
    */
-  router.post("/signup", vaild.auth.valid_signup, ctrl.auth.post_signup);
+  router.post("/signup", vaild.auth.post_signup, ctrl.auth.post_signup);
 
   /**
    * @description 로그인
@@ -21,18 +21,18 @@ function authRouter(root) {
    * @description 이메일 인증번호 발송
    * @routes POST /auth/send
    */
-  router.post("/send", vaild.auth.valid_send, ctrl.auth.post_send);
+  router.post("/send", vaild.auth.post_send, ctrl.auth.post_send);
 
   /**
    * @description 이메일 인증번호 확인
    * @routes POST /auth/confirm
    */
-  router.post("/confirm", vaild.auth.valid_confirm, ctrl.auth.post_confirm);
+  router.post("/confirm", vaild.auth.post_confirm, ctrl.auth.post_confirm);
 
   /**
    * @description 임시 비밀번호 발급
    * @routes POST /auth/temppwd
    */
-  router.post("/temp", vaild.auth.valid_temp, ctrl.auth.post_temp);
+  router.post("/temp", vaild.auth.post_temp, ctrl.auth.post_temp);
 }
 module.exports = authRouter;
