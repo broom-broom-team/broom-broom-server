fd2da7aa6131e8a621b175783a839810ac714440
윤철중 <cjfwnd123zz@gmail.com>
2021-07-28 21:30:12 +0900

    add: 심부름작성 이미지 업로드 추가

--- controllers/post.ctrl.js
+++ controllers/post.ctrl.js
@@ -1,19 +1,27 @@
 const model = require("../models");
+const postDefault = "https://broombroom.s3.ap-northeast-2.amazonaws.com/broomPost-default.png";
 
 exports.post_post = async (req, res, next) => {
   const { title, description, price, deadline, requiredTime } = req.body;
   try {
+    const images = req.files;
+    const imagesURI = images.length != 0 ? images.map((img) => img.location) : postDefault;
+    const postImageURI = imagesURI.toString();
     const userAddress = await model.UserAddress.findOne({ where: { userId: req.user.id } });
     if (userAddress) {
-      await model.Post.create({
-        title,
-        description,
-        price,
-        deadline,
-        requiredTime,
-        sellerId: req.user.id,
-        sellingDistrict: userAddress.districtId,
-      });
+      await model.Post.create(
+        {
+          title,
+          description,
+          price,
+          deadline,
+          requiredTime,
+          sellerId: req.user.id,
+          sellingDistrict: userAddress.districtId,
+          PostImages: [{ postImageURI }],
+        },
+        { include: [{ model: model.PostImage }] }
+      );
       return res.status(200).json({ success: true, message: "심부름 등록이 완료되었습니다." });
     } else {
       return res.status(400).json({ success: true, message: "회원님의 주소를 찾을 수 없는 에러가 발생하였습니다." });
--- validators/post.valid.js
+++ validators/post.valid.js
@@ -1,33 +1,84 @@
 const title_regex = /^.{4,40}$/;
 const description_regex = /^.{8,400}$/;
 const now = Date.now();
+const s3 = require("../config/s3.config");
 
 exports.post_post = (req, res, next) => {
   // TODO: requiredTime 유효성검사
   // TODO: 코드 좀 깔끔하게 수정할 것 (다른 파일의 코드들도 마찬가지)
   const { title, price, description, deadline } = req.body;
   const parseDeadline = new Date(deadline);
-  if (!title_regex.test(title)) {
-    return res.status(400).json({ success: false, message: "심부름 제목은 최소 4글자, 최대 40글자까지만 입력가능합니다." });
+  const images = req.files;
+  const imagesURI = images.map((img) => img.key);
+  const imagesObjects = [];
+  for (let i = 0; i < imagesURI.length; i++) {
+    imagesObjects.push({ Key: imagesURI[i] });
   }
-  if (isNaN(parseDeadline.getTime())) {
-    return res.status(400).json({ success: false, message: "마감시간은 숫자만 입력가능합니다." });
+  if (!images) {
+    if (!title_regex.test(title)) {
+      return res.status(400).json({ success: false, message: "심부름 제목은 최소 4글자, 최대 40글자까지만 입력가능합니다." });
+    }
+    if (isNaN(parseDeadline.getTime())) {
+      return res.status(400).json({ success: false, message: "마감시간은 숫자만 입력가능합니다." });
+    } else {
+      if (parseDeadline < now) {
+        return res.status(400).json({ success: false, message: "마감시간은 현재시간보다 이후 시간만 입력가능합니다." });
+      }
+      if (parseDeadline.getFullYear() > 2099) {
+        return res.status(400).json({ success: false, message: "마감시간의 년도는 2099년도 이전으로만 입력가능합니다." });
+      }
+    }
+    if (!description_regex.test(description)) {
+      return res.status(400).json({ success: false, message: "심부름 내용은 최소 8글자, 최대 400글자까지만 입력가능합니다." });
+    }
+    if (price < 1000) {
+      return res.status(400).json({ success: false, message: "가격은 1000원이상부터 입력가능합니다." });
+    }
+    if (price % 10 != 0) {
+      return res.status(400).json({ success: false, message: "가격의 최소단위는 10원입니다." });
+    }
+    next();
   } else {
-    if (parseDeadline < now) {
-      return res.status(400).json({ success: false, message: "마감시간은 현재시간보다 이후 시간만 입력가능합니다." });
+    try {
+      if (!title_regex.test(title)) {
+        return s3.deleteObjects({ Bucket: "broombroom", Delete: { Objects: imagesObjects } }, () => {
+          res.status(400).json({ success: false, message: "심부름 제목은 최소 4글자, 최대 40글자까지만 입력가능합니다." });
+        });
+      }
+      if (isNaN(parseDeadline.getTime())) {
+        return s3.deleteObjects({ Bucket: "broombroom", Delete: { Objects: imagesObjects } }, () => {
+          res.status(400).json({ success: false, message: "마감시간은 숫자만 입력가능합니다." });
+        });
+      } else {
+        if (parseDeadline < now) {
+          return s3.deleteObjects({ Bucket: "broombroom", Delete: { Objects: imagesObjects } }, () => {
+            res.status(400).json({ success: false, message: "마감시간은 현재시간보다 이후 시간만 입력가능합니다." });
+          });
+        }
+        if (parseDeadline.getFullYear() > 2099) {
+          return s3.deleteObjects({ Bucket: "broombroom", Delete: { Objects: imagesObjects } }, () => {
+            res.status(400).json({ success: false, message: "마감시간의 년도는 2099년도 이전으로만 입력가능합니다." });
+          });
+        }
+      }
+      if (!description_regex.test(description)) {
+        return s3.deleteObjects({ Bucket: "broombroom", Delete: { Objects: imagesObjects } }, () => {
+          res.status(400).json({ success: false, message: "심부름 내용은 최소 8글자, 최대 400글자까지만 입력가능합니다." });
+        });
+      }
+      if (price < 1000) {
+        return s3.deleteObjects({ Bucket: "broombroom", Delete: { Objects: imagesObjects } }, () => {
+          res.status(400).json({ success: false, message: "가격은 1000원이상부터 입력가능합니다." });
+        });
+      }
+      if (price % 10 != 0) {
+        return s3.deleteObjects({ Bucket: "broombroom", Delete: { Objects: imagesObjects } }, () => {
+          res.status(400).json({ success: false, message: "가격의 최소단위는 10원입니다." });
+        });
+      }
+      next();
+    } catch (e) {
+      return next(e);
     }
-    if (parseDeadline.getFullYear() > 2099) {
-      return res.status(400).json({ success: false, message: "마감시간의 년도는 2099년도 이전으로만 입력가능합니다." });
-    }
   }
-  if (!description_regex.test(description)) {
-    return res.status(400).json({ success: false, message: "심부름 내용은 최소 8글자, 최대 400글자까지만 입력가능합니다." });
-  }
-  if (price < 1000) {
-    return res.status(400).json({ success: false, message: "가격은 1000원이상부터 입력가능합니다." });
-  }
-  if (price % 10 != 0) {
-    return res.status(400).json({ success: false, message: "가격의 최소단위는 10원입니다." });
-  }
-  next();
 };
