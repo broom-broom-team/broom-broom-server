c8cd7544e1e74a574a67fac316fcc874fdaafb9d
윤철중 <cjfwnd123zz@gmail.com>
2021-08-02 13:38:13 +0900

    add: 내가 등록한 심부름 확인 기능 추가

--- controllers/post.ctrl.js
+++ controllers/post.ctrl.js
@@ -157,3 +157,40 @@ exports.post_edit = async (req, res, next) => {
     return next(e);
   }
 };
+
+exports.get_history_me = async (req, res, next) => {
+  try {
+    const posts = await model.Post.findAll({
+      where: { sellerId: req.user.id },
+      order: [["updatedAt", "DESC"]],
+      attributes: ["id", "title", "deadline", "requiredTime", "updatedAt", "price", "status"],
+      include: [
+        { model: model.District, attributes: ["simpleAddress"] },
+        { model: model.PostImage, attributes: ["postImageURI"] },
+      ],
+    });
+    if (posts.length === 0) {
+      return res.status(200).json({ success: true, message: "아직 작성한 심부름이 없습니다." });
+    } else {
+      const history = [];
+      for (let i = 0; i < posts.length; i++) {
+        let postImageURI = posts[i].PostImages[0].postImageURI.split(",");
+        let post = {
+          id: posts[i].id,
+          title: posts[i].title,
+          deadline: posts[i].deadline,
+          requiredTime: posts[i].requiredTime,
+          updatedAt: posts[i].updatedAt,
+          price: posts[i].price,
+          status: posts[i].status,
+          simpleAddress: posts[i].District.simpleAddress,
+          thumbnail: postImageURI[0],
+        };
+        history.push(post);
+      }
+      return res.status(200).json({ success: true, message: "내가 작성한 심부름을 불러옵니다.", history });
+    }
+  } catch (e) {
+    return next(e);
+  }
+};
