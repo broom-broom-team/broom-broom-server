ee95aaca1025ca51674bcad2a28e35667e54ad58
윤철중 <cjfwnd123zz@gmail.com>
2021-08-09 18:31:52 +0900

    add: 메인 페이지에 들어갈 심부름 불러오기 추가

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
+const main = require("../controllers/main.ctrl");
 const auth = require("../controllers/auth.ctrl");
 const address = require("../controllers/address.ctrl");
 const user = require("../controllers/user.ctrl");
 const post = require("../controllers/post.ctrl");
 
-module.exports = { auth, address, user, post };
+module.exports = { main, auth, address, user, post };
